f65a631ba3f2c49bbb97c2e12521dd7d8db90151
9999lpt-source <9999lpt@gmail.com>
2026-07-21 19:37:10 +0700

    Fix bug missing numpy for PyAV

--- services/tts_services.py
+++ services/tts_services.py
@@ -1,7 +1,6 @@
 import edge_tts
 import av
 
-# Giọng đọc Tiếng Việt chuẩn của Microsoft Edge
 VOICE = "vi-VN-HoaiMyNeural"
 
 class EdgeTTSService:
@@ -9,10 +8,6 @@ class EdgeTTSService:
         pass
 
     async def stream_tts_pcm(self, text: str, chunk_size: int = 2048):
-        """
-        Stream dữ liệu MP3 từ Edge-TTS, giải mã sang PCM (16kHz, Mono, 16-bit Little-Endian)
-        và yield từng khối PCM (chunk_size = 2048 bytes) xuống ESP32 theo thời gian thực.
-        """
         if not text or not text.strip():
             return
 
@@ -29,8 +24,6 @@ class EdgeTTSService:
             
             pcm_buffer = bytearray()
 
-            print("🔊 [EDGE TTS]: Đang stream và giải mã PCM chuẩn s16le...", flush=True)
-
             async for chunk in communicate.stream():
                 if chunk["type"] == "audio":
                     packets = codec.parse(chunk["data"])
@@ -40,16 +33,13 @@ class EdgeTTSService:
                             # Convert frame về 16000Hz trước khi lấy bytes
                             resampled_frames = resampler.resample(frame)
                             for r_frame in resampled_frames:
-                                # 💡 SỬA LỖI RÈ TIẾNG: Ép kiểu mảng NumPy về int16 Little-Endian ('<i2')
-                                # Giúp ESP32 hiểu đúng thứ tự Byte, âm thanh sẽ cực kỳ trong rõ!
-                                raw_pcm = r_frame.to_ndarray().astype('<i2').tobytes()
+                                raw_pcm = r_frame.to_ndarray().tobytes()
                                 pcm_buffer.extend(raw_pcm)
 
                                 while len(pcm_buffer) >= chunk_size:
                                     yield bytes(pcm_buffer[:chunk_size])
                                     del pcm_buffer[:chunk_size]
 
-            # Gửi nấc dữ liệu PCM còn thừa ở cuối (nếu có)
             if len(pcm_buffer) > 0:
                 yield bytes(pcm_buffer)
 
