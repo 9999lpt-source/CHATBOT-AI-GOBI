529642d4b5608794ea22351014a0e1bc989a2555
9999lpt-source <9999lpt@gmail.com>
2026-07-23 18:24:24 +0700

    Cap nhat tts

--- services/tts_services.py
+++ services/tts_services.py
@@ -15,13 +15,6 @@ class EdgeTTSService:
             communicate = edge_tts.Communicate(text, VOICE)
             codec = av.CodecContext.create('mp3', 'r')
             
-            # Khởi tạo bộ Chuyển đổi Sample Rate về đúng 16000Hz, Mono, 16-bit
-            resampler = av.AudioResampler(
-                format='s16',       # 16-bit PCM
-                layout='mono',      # 1 kênh Mono
-                rate=16000          # Ép về 16000 Hz cho ESP32
-            )
-            
             pcm_buffer = bytearray()
 
             async for chunk in communicate.stream():
@@ -30,15 +23,13 @@ class EdgeTTSService:
                     for packet in packets:
                         frames = codec.decode(packet)
                         for frame in frames:
-                            # Convert frame về 16000Hz trước khi lấy bytes
-                            resampled_frames = resampler.resample(frame)
-                            for r_frame in resampled_frames:
-                                raw_pcm = r_frame.to_ndarray().tobytes()
-                                pcm_buffer.extend(raw_pcm)
+                            # Lấy trực tiếp raw bytes từ frame gốc (không qua resampler)
+                            raw_pcm = frame.to_ndarray().tobytes()
+                            pcm_buffer.extend(raw_pcm)
 
-                                while len(pcm_buffer) >= chunk_size:
-                                    yield bytes(pcm_buffer[:chunk_size])
-                                    del pcm_buffer[:chunk_size]
+                            while len(pcm_buffer) >= chunk_size:
+                                yield bytes(pcm_buffer[:chunk_size])
+                                del pcm_buffer[:chunk_size]
 
             if len(pcm_buffer) > 0:
                 yield bytes(pcm_buffer)
